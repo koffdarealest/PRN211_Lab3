085b91b0def633ecbe11f7b90e6753534bd101ae
koffdarealest <dinhsontung153153@gmail.com>
2024-03-13 15:34:50 +0700

    add git

---
+++ AutomobileLibrary/Repository/CarRepository.cs
@@ -0,0 +1,22 @@
+﻿using AutomobileLibrary.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomobileLibrary.Repository
+{
+    public class CarRepository : ICarRepository
+    {
+        public void DeleteCar(int carID) => CarDAO.Instance.Remove(carID);
+
+        public IEnumerable<Car> GetCars() => CarDAO.Instance.GetCarList();
+
+        public Car GetCarByID(int carID) => CarDAO.Instance.GetCarByID(carID);
+
+        public void InsertCar(Car car) => CarDAO.Instance.AddNew(car);
+
+        public void UpdateCar(Car car) => CarDAO.Instance.Update(car);
+    }
+}
